fd82fb17cbe23729e3f31f45cb95e33eee82b0e5
箱猫 <kagawa0@gmail.com>
2016-03-23 01:56:57 +0900

    Add BLE connect.

--- BLE_test/BLE_test/Source/BLEMainViewController.m
+++ BLE_test/BLE_test/Source/BLEMainViewController.m
@@ -16,12 +16,14 @@
 }
 
 @property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheralManager *peripheralManager;
 
 @end
 
 @implementation BLEMainViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [logLabel setText:@"起動したよ"];
@@ -31,10 +33,33 @@
     
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - Connect 関連
+
+// 接続開始
+- (void)startConnect:(CBPeripheral *)peripheral
+{
+    [logLabel setText:@"接続開始"];
+    [self.centralManager connectPeripheral:peripheral
+                                   options:nil];
+}
+
+- (void)centralManager:(CBCentralManager *)central
+  didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    [logLabel setText:@"接続成功"];
+}
+
+- (void)centralManager:(CBCentralManager *)central
+didFailToConnectPeripheral:(CBPeripheral *)peripheral
+                 error:(NSError *)error
+{
+    [logLabel setText:@"接続失敗"];
+}
 
 #pragma mark - Search 関連
 
@@ -48,16 +73,19 @@
 - (void)centralManager:(CBCentralManager *)central
  didDiscoverPeripheral:(CBPeripheral *)peripheral
      advertisementData:(NSDictionary<NSString *,id> *)advertisementData
-                  RSSI:(NSNumber *)RSSI {
+                  RSSI:(NSNumber *)RSSI
+{
     [logLabel setText:[NSString stringWithFormat:@"発見したBLEデバイス :%@", peripheral]];
     
     // 自動的に止まらないので止めておく
+    // TODO 多数の接続を許可する
     [self.centralManager stopScan];
+    
+    [self startConnect:peripheral];
 }
 
-
-- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
-    
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
     switch (central.state) {
         case CBCentralManagerStatePoweredOn:
             [logLabel setText:@"動作可能"];
@@ -79,6 +107,5 @@
             break;
     }
 }
-
 
 @end
